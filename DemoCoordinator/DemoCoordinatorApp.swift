8dd51ed441b712c723523a138a0b0420590295fd
Joanna Vigne <joanna.vigne@outlook.fr>
2023-05-04 23:23:36 +0200

    Initial Commit

---
+++ DemoCoordinator/DemoCoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCoordinatorApp.swift
+//  DemoCoordinator
+//
+//  Created by Joanna Vigne on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoCoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
